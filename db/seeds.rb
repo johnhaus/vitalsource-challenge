6076d5565117d487ea689135a28de3b6ca7a4760
johnhaus <john.haus@outlook.com>
2019-11-02 14:50:46 -0500

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+User.destroy_all
+
+puts 'Creating users...'
+user = User.new
+user.name = 'Jim'
+user.email = 'jim@email.com'
+user.password = '111111'
+user.sign_in_count = 4
+user.save!
+
+user = User.new
+user.name = 'Bob'
+user.email = 'bob@email.com'
+user.password = '222222'
+user.sign_in_count = 9
+user.save!
+
+user = User.new
+user.name = 'Tom'
+user.email = 'tom@email.com'
+user.password = '333333'
+user.sign_in_count = 0
+user.save!
+
+puts 'Finished'
